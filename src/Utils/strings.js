7dfaa95c014c4549226e82cf6a0e0a46d13218bf
Pranay <kyatham.pranay@gmail.com>
2024-01-18 13:51:20 -0800

    Updates

--- src/Utils/strings.js
+++
@@ -1,3 +0,0 @@
-export const weekNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-export const monthNames = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
